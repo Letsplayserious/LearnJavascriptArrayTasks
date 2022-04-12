170a098c7d086f44d0aea00a4aa3811050073079
S-aint <aintlessmore@gmail.com>
2022-04-12 09:52:15 +0300

    Added buffer

---
+++ prototypes.js
@@ -0,0 +1,25 @@
+// У нас есть два хомяка: шустрый (speedy) и ленивый (lazy); оба наследуют от общего объекта hamster.
+// Когда мы кормим одного хомяка, второй тоже наедается. Почему? Как это исправить?
+
+let hamster = {
+    eat(food) {
+      if(!this.stomach){
+        this.stomach;
+        this.stomach = [];
+        this.stomach.push(food);
+      }
+      else{
+      this.stomach.push(food);
+        }
+    }
+  };
+
+  let speedy = {
+    __proto__: hamster
+  };
+
+  let lazy = {
+    __proto__: hamster
+  };
+
+  speedy.eat("apple");
