d5e164aac30d70e265b51b148456908466a71827
Ионов Святослав <s.ionov@corp.fsk-tech>
2022-06-02 17:24:25 +0300

    Ajax

--- AJAX Test/post.js
+++ AJAX Test/post.js
@@ -39,24 +39,141 @@ postButton.addEventListener("click", () => {
 });
 
 function specialHttpRequest({method, url} = {}, callback) {
-    const xhr = new XMLHttpRequest();
-    xhr.open(method, url);
-    xhr.addEventListener('load', ()=>{
-        const response = JSON.parse(xhr.responseText);
-        callback(response);
-    });
+    try{
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, url);
+        xhr.addEventListener('load', ()=>{
+            if(Math.floor(xhr.status/100)!==2){
+                callback(`Error. Status code: ${xhr.status}`, xhr);
+                return;
+            }
+            else{
+                const response = JSON.parse(xhr.responseText);
+                callback(null, response);
+            }
+        });
 
-    xhr.addEventListener("error", ()=>{
-        console.log("error");
-    })
+        xhr.addEventListener("error", ()=>{
+            console.log("error");
+        })
 
-    xhr.send();
+        xhr.send();
+    } catch(error){
+        callback(error);
+    }
 }
 
-specialHttpRequest({
-    method: "GET",
-    url: "https://jsonplaceholder.typicode.com/posts",
+// specialHttpRequest({
+//     method: "GET",
+//     url: "https://jsonplaceholder.typicode.com/posts",
 
-}, res => {
-    console.log(res);
-})
+// }, (err, res) => {
+//     if(err){
+//         console.log(err);
+//         return;
+//     }
+//     console.log(res);
+// })
+
+function http(){
+    return{
+        get(url, callback){
+            try{
+                const xhr = new XMLHttpRequest();
+                xhr.open("GET", url);
+                xhr.addEventListener('load', ()=>{
+                    if(Math.floor(xhr.status/100)!==2){
+                        callback(`Error. Status code: ${xhr.status}`, xhr);
+                        return;
+                    }
+                    else{
+                        const response = JSON.parse(xhr.responseText);
+                        callback(null, response);
+                    }
+                });
+
+                xhr.addEventListener("error", ()=>{
+                    console.log("errorZ");
+                })
+
+                xhr.send();
+            } catch(error){
+                callback(error);
+            }
+        },
+        post(url, body, headers, callback){
+            try{
+                const xhr = new XMLHttpRequest();
+                xhr.open("POST", url);
+                xhr.addEventListener('load', ()=>{
+                    if(Math.floor(xhr.status/100)!==2){
+                        callback(`Error. Status code: ${xhr.status}`, xhr);
+                        return;
+                    }
+                    else{
+                        const response = JSON.parse(xhr.responseText);
+                        callback(null, response);
+                    }
+                });
+
+                xhr.addEventListener("error", ()=>{
+                    console.log("error");
+                });
+
+                if (headers){
+                    Object.entries(headers).forEach(([key, value]) => {
+                        xhr.setRequestHeader(key, value);
+                    })
+                }
+
+                xhr.send(JSON.stringify(body));
+            } catch(error){
+                callback(error);
+            }
+        }
+    }
+}
+
+const myHttp = http();
+myHttp.post(`https://jsonplaceholder.typicode.com/posts`, myObject, {"Content-type": "application/json"}, (err, res) => {console.log(err,res)});
+
+
+
+function getPost(id){
+    return new Promise((resolve, reject) => {
+        myHttp.get(`https://jsonplaceholder.typicode.com/posts/1`, (err, res) =>{
+            if(err){
+                reject();
+            }
+            resolve(res);
+        })
+    });
+}
+
+function getPostComments(){
+    return new Promise((resolve, reject) => {
+        myHttp.get(`https://jsonplaceholder.typicode.com/comments?postId=1`, (err, res) => {
+            if(err){
+                reject();
+            }
+            resolve(res);
+        });
+    });
+}
+
+function getUserCreatedPost(){
+    return new Promise((resolve, reject)=>{
+        myHttp.get(`https://jsonplaceholder.typicode.com/comments?postId=1`, (err, res) =>{
+            if(err){
+                reject();
+            }
+            resolve(res);
+        })
+    })
+}
+
+getPost()
+.then(post => console.log(post))
+.then(comments => getPostComments(comments))
+.then(user => console.log(user))
+.catch(err => console.log(err));
